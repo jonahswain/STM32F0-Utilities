996b80c430101e1136f4cbc5287b84af0042b9fa
Jonah Swain <jonahswain@gmail.com>
2018-05-04 08:22:06 +0200

    Added C template

---
+++ STM32F0-Utilities/STM32F0-Utilities/src/__TEMPLATE.c
@@ -0,0 +1,40 @@
+#pragma once
+/*
+STM32F0 Utilities
+A Collection of utilities for STM32F0 microcontrollers, primarily targeted at the STM32F051C6-based UCT development board
+
+Author: Jonah Swain (SWNJON003)
+Date created: /2018
+Date modified: /2018
+
+Module:
+
+
+*/
+
+/* INCLUDES */
+
+#ifndef STM32F0XX_H
+#include "stm32f0xx.h"
+#define STM32F0XX_H
+#endif
+
+#ifndef STDINT_H
+#include <stdint.h>
+#define STDINT_H
+#endif
+
+#ifndef STM32F0_GPIO_H
+#include "STM32F0_GPIO.h"
+#define STM32F0_GPIO_H
+#endif
+
+#ifndef STM32F0_OTHER_H
+#include "STM32F0_OTHER.h"
+#define STM32F0_OTHER_H
+#endif
+
+/* CONSTANT DEFINITIONS */
+
+
+/* FUNCTIONS */
