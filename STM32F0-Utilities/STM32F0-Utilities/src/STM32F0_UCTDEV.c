f7426411c1d11302af97340b271c4ac94965dd94
Jonah Swain <jonahswain@gmail.com>
2018-05-23 11:15:48 +0200

    First attempt at interfacing with the EEPROM using SPI

--- STM32F0-Utilities/STM32F0-Utilities/src/STM32F0_UCTDEV.c
+++ STM32F0-Utilities/STM32F0-Utilities/src/STM32F0_UCTDEV.c
@@ -4,7 +4,7 @@ A Collection of utilities for STM32F0 microcontrollers, primarily targeted at th
 
 Author: Jonah Swain (SWNJON003)
 Date created: 17/04/2018
-Date modified: 30/04/2018
+Date modified: 23/05/2018
 
 Module: UCTDEV
 
@@ -74,6 +74,21 @@ void init_tempSensor() {
 	init_I2C(I2C2, 1, 0xC7, 0xC2, 0x04, 0x02); // Initialise I2C2 with the required timing settings for the module
 }
 
+void init_EEPROM() {
+	// Initialise SPI for the EEPROM
+	pinMode(EEPROM_MOSI, GPIO_ALTFN); // Set MOSI as alternate function
+	pinMode(EEPROM_MISO, GPIO_ALTFN); // Set MISO as alternate function
+	pinMode(EEPROM_SCK, GPIO_ALTFN); // Set SCK as alternate functionm
+	pinMode(EEPROM_CS, GPIO_OUTPUT); // Set chip select as output
+	afSelect(EEPROM_MOSI, GPIO_AF0); // Map MOSI to AF0 (SPI)
+	afSelect(EEPROM_MISO, GPIO_AF0); // Map MISO to AF0 (SPI)
+	afSelect(EEPROM_SCK, GPIO_AF0); // Map SCK to AF0 (SPI)
+	digitalWrite(EEPROM_CS, HIGH); // Set CS HIGH
+
+	init_SPI(SPI2, SPI_BAUD_3MHZ, SPI_MASTER_MODE, SPI_MSB_FIRST, SPI_FRAMESIZE_8BIT, 0, 0, 0, 0, 0, 1); // Initialise and configure SPI in 8-bit mode 0,0
+
+}
+
 void ledWrite(uint8_t pattern) {
 	// Displays a pattern on the red LEDs on the board
 	GPIOB->ODR &= 0xFFFF & pattern; // Turn off the LEDs that need to be turned off
@@ -91,4 +106,69 @@ uint8_t tempSensorRead() {
 	unsigned char temperature = 0;
 	i2cReadFromSlave(I2C2, &temperature, 1, I2C_7BIT_ADDRESSING, TS_READ_ADDRESS, TS_READ_ADDRESS, 0x00); // Read the temperature sensor
 	return temperature;
+}
+
+void eepromWrite(uint16_t address, uint8_t data) {
+	// Writes a byte of data to an address in the EEPROM
+	uint8_t dummyData; // A variable to store any rubbish that is received
+	__spiFlushRXBuffer(SPI2); // Flush RX buffer before starting
+
+	// Set the write enable latch
+	digitalWrite(EEPROM_CS, LOW); // Set chip select low
+	__cpuHoldDelay(1);
+	spiTransmitFrame(SPI2, EEPROM_WREN); // Send the EEPROM write enable instruction
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging the request
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+	digitalWrite(EEPROM_CS, HIGH); // Set chip select high
+	__cpuHoldDelay(5000); // Wait for instruction to complete
+
+	// Send the write instruction
+	digitalWrite(EEPROM_CS, LOW); // Set chip select low
+	__cpuHoldDelay(1);
+	spiTransmitFrame(SPI2, EEPROM_WRITE); // Send the EEPROM write instruction
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging the request
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+
+	// Send the address
+	spiTransmitFrame(SPI2, (address >> 8)); // Send the address MSBs
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+	spiTransmitFrame(SPI2, (address & 0x00FF)); // Send the address LSBs
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+
+	// Send the data
+	spiTransmitFrame(SPI2, data); // Send the data
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+
+	digitalWrite(EEPROM_CS, HIGH); // Set chip select high
+	__cpuHoldDelay(5000); // Wait for instruction to complete
+}
+
+uint8_t eepromRead(uint16_t address) {
+	// Reads a byte of data from an address in the EEPROM
+	uint8_t dummyData; // A variable to store any rubbish that is received
+	__spiFlushRXBuffer(SPI2); // Flush RX buffer before starting
+
+	// Send the read instruction
+	digitalWrite(EEPROM_CS, LOW); // Set chip select low
+	__cpuHoldDelay(1);
+	spiTransmitFrame(SPI2, EEPROM_READ); // Send the EEPROM write instruction
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging the request
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+
+	// Send the address
+	spiTransmitFrame(SPI2, (address >> 8)); // Send the address MSBs
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+	spiTransmitFrame(SPI2, (address & 0x00FF)); // Send the address LSBs
+	while (!(SPI2->SR & SPI_SR_RXNE)); // Wait for the EEPROM to send rubbish, acknowledging
+	dummyData = *((uint8_t*)(&SPI2->DR)); // Pull the rubbish from the RX buffer
+
+	// Get the data
+	uint8_t data = spiReceiveFrame(SPI2); // Get the data
+
+	digitalWrite(EEPROM_CS, HIGH); // Set chip select high
+	__cpuHoldDelay(5000); // Wait for instruction to complete
 }
