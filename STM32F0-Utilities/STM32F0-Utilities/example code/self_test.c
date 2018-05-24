8f7c2f36500fc5be7754ddbd56be9f2540cdb43e
Jonah Swain <jonahswain@gmail.com>
2018-05-24 22:58:42 +0200

    Added EEPROM test

--- STM32F0-Utilities/STM32F0-Utilities/example code/self_test.c
+++ STM32F0-Utilities/STM32F0-Utilities/example code/self_test.c
@@ -8,6 +8,7 @@ Date modified: 24/05/2018
 
 Self Test
 Example firmware to load on to the board to do a full peripheral self-test and demonstrate the features
+Rename this file to 'main.c'
 
 */
 
@@ -29,8 +30,61 @@ Example firmware to load on to the board to do a full peripheral self-test and d
 #endif
 
 /* CONSTANT DEFINITIONS */
+#define EEPROM_TESTDATA_OFFSET 0 // Address in EEPROM to store test data
+#define EEPROM_TESTCOMPL_OFFSET 8189 // Address in EEPROM to store all tests complete flag
+#define EEPROM_SIG_OFFSET 8190 // Address in EEPROM to place signature after running tests
 
+/* GLOBAL VARIABLES */
+uint8_t eepromTestData[] = { 0xEE, 0xE2, 0x04, 0x6F }; // EEPROM test data
+uint8_t eepromSignature[] = { 'J', 'S' }; // A personal touch
+uint8_t ledPattern = 0; // Pattern to display on LEDs
+uint8_t ledDirection = 0; // Direction to move LED pattern (initial stage)
+int mode = 0; // Testing mode
+uint8_t menuPosition = 0;
 
 /* FUNCTIONS */
 
+void fullSelfTest() {
+	// The full complement of self tests
+}
+
+void menu() {
+	// Menu of self-tests to display on LCD
+}
+
+int testEEPROMWrite() {
+	// Write test data to EEPROM
+	uint16_t address; // EEPROM address
+	for (uint16_t i = 0; i < 4; i++) {
+		address = EEPROM_TESTDATA_OFFSET + i;
+		eepromWrite(address, eepromTestData[i]); // Write data to EEPROM
+		if (eepromRead(address) != eepromTestData[i]) { // Verify data
+			return 0; // EEPROM data does not match
+		}
+	}
+	return 1; // Writing complete
+}
+
+int testEEPROMRead() {
+	// Read, verify, and erase test data from EEPROM
+	uint16_t address; // EEPROM address
+	for (uint16_t i = 0; i < 4; i++) {
+		address = EEPROM_TESTDATA_OFFSET + i;
+		if (eepromRead(address) != eepromTestData[i]) { // Check to see if data matches
+			return 0;
+		}
+		eepromWrite(address, 0); // Erase data
+	}
+	return 1;
+}
+
 /* MAIN FUNCTION */
+void main() {
+	// Setup code, to run once
+
+
+	// Main loop
+	while(1) {
+
+	}
+}
