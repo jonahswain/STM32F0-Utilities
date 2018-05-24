f09e7c609f67411526b1b802dde009d485caf44c
Jonah Swain <jonahswain@gmail.com>
2018-05-24 21:27:41 +0200

    Initial Commit for self-test branch

---
+++ STM32F0-Utilities/STM32F0-Utilities/example code/self_test.c
@@ -0,0 +1,36 @@
+/*
+STM32F0 Utilities
+A Collection of utilities for STM32F0 microcontrollers, primarily targeted at the STM32F051C6-based UCT development board
+
+Author: Jonah Swain (SWNJON003)
+Date created: 24/05/2018
+Date modified: 24/05/2018
+
+Self Test
+Example firmware to load on to the board to do a full peripheral self-test and demonstrate the features
+
+*/
+
+/* INCLUDES */
+
+#ifndef STM32F0XX_H
+#include "stm32f0xx.h"
+#define STM32F0XX_H
+#endif
+
+#ifndef STDINT_H
+#include <stdint.h>
+#define STDINT_H
+#endif
+
+#ifndef STM32F0_UCTDEV_H
+#include "STM32F0_UCTDEV.h"
+#define STM32F0_UCTDEV_H
+#endif
+
+/* CONSTANT DEFINITIONS */
+
+
+/* FUNCTIONS */
+
+/* MAIN FUNCTION */
