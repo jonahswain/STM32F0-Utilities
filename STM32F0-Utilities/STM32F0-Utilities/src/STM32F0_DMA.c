4e38fbf9cf55816aa2a3e278267c2e82459b84df
Jonah Swain <jonahswain@gmail.com>
2018-05-26 12:17:27 +0200

    Initial commit - added DMA source files

---
+++ STM32F0-Utilities/STM32F0-Utilities/src/STM32F0_DMA.c
@@ -0,0 +1,20 @@
+/*
+STM32F0 Utilities
+A Collection of utilities for STM32F0 microcontrollers, primarily targeted at the STM32F051C6-based UCT development board
+
+Author: Jonah Swain (SWNJON003)
+Date created: 26/05/2018
+Date modified: 26/05/2018
+
+Module: DMA
+Functions for configuring/controlling the direct memory access module
+
+*/
+
+/* INCLUDES */
+#ifndef STM32F0_DMA_H
+#include "STM32F0_DMA.h"
+#define STM32F0_DMA_H
+#endif
+
+/* FUNCTIONS */
